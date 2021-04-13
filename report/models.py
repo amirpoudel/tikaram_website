ba7e3c844cb12ef75f945f316159343640c28359
amirpoudel <amirpoudel2058@gmail.com>
2021-04-13 08:04:27 +0545

    one step ahead of final

--- report/models.py
+++ report/models.py
@@ -4,3 +4,11 @@ class Report(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
     files = models.FileField(upload_to='report/media/')
+
+
+    def __str__(self):
+        return self.title
+
+
+
+
